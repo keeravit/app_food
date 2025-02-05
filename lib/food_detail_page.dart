e7a8547f01158b1535e9bd3dc542fb732a7557ef
keeravit <keeravit8862@gmail.com>
2025-02-05 11:35:34 +0700

    Update food_detail_page.dart

--- lib/food_detail_page.dart
+++ lib/food_detail_page.dart
@@ -34,7 +34,7 @@ class FoodDetailPage extends StatelessWidget {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => _launchURL(food['youtubeLink']!),
-              chil: const Text('ไปที่ YouTube'),
+              child: const Text('ไปที่ YouTube'),
             ),
           ],
         ),
